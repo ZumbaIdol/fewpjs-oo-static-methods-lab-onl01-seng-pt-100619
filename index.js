e08a57f118c6471a7a712567e714072492ab5d74
ZumbaIdol <david.a.foote@gmail.com>
2020-05-27 15:48:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(sentence) {
+    
+  }
 }
